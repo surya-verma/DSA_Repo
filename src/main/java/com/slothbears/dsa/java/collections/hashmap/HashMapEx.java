dca1f0621eea6966924f95ea136154d65f1e66b4
suryaverma <suryakv14@outlook.com>
2020-03-25 09:42:14 -0700

    Commit

---
+++ src/main/java/com/slothbears/dsa/java/collections/hashmap/HashMapEx.java
@@ -0,0 +1,30 @@
+package com.slothbears.dsa.java.collections.hashmap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapEx {
+
+	public static void main(String[] args) {
+		
+		HashMap<Integer, String> mapObj = new HashMap<Integer, String>();
+		
+		mapObj.put(1, "Surya");
+		mapObj.put(3, "Raju");
+		mapObj.put(2, "Harry");
+		
+		
+		
+		Set set = mapObj.entrySet();
+		Iterator itr = set.iterator();
+		
+		while(itr.hasNext()) {
+			Map.Entry mentry = (Map.Entry) itr.next();
+			System.out.println(mentry.getKey());
+			System.out.println(mentry.getValue());			
+		}
+	}
+
+}
---
+++ src/main/java/com/slothbears/dsa/java/collections/streams/StreamOfObjects.java
@@ -0,0 +1,26 @@
+package com.slothbears.dsa.java.collections.streams;
+
+import java.util.Comparator;
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamOfObjects {
+
+	public static void main(String[] args) {
+		
+		Employee[] arrayOfEmps = {
+			    new Employee(1, "Jeff Bezos", 100000.0), 
+			    new Employee(2, "Bill Gates", 200000.0), 
+			    new Employee(3, "Mark Zuckerberg", 300000.0)
+			};
+
+		Supplier<Stream<Employee>> empStreamSupplier = () -> Stream.of(arrayOfEmps);
+		
+		empStreamSupplier.get().forEach(employee -> System.out.println(employee.toString()));
+		
+		//empStreamSupplier.get().collect(Collectors.toMap(e -> e.empid, e ->e, BinaryOperator.maxBy(Comparator.comparingDouble(e->e.salary))));
+	}
+
+}
